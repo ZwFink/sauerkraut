945cb835f783e4cd4270b04003f9e89405f072b6
Zane Fink <finkzane@gmail.com>
2024-11-15 16:21:04 -0600

    Runnable frames (#4)

--- migrames/migrames.C
+++ migrames/migrames.C
@@ -208,6 +208,33 @@ PyFrameObject *create_copied_frame(PyThreadState *tstate, _PyInterpreterFrame *t
     return new_frame;
 }
 
+PyFrameObject *push_frame_for_running(PyThreadState *tstate, _PyInterpreterFrame *to_push, PyCodeObject *code) {
+    _PyInterpreterFrame *stack_frame = ThreadState_PushFrame(tstate, code->co_framesize);
+    py_weakref<PyFrameObject> pyframe_object = to_push->frame_obj;
+    if(stack_frame == NULL) {
+        PySys_WriteStderr("<Migrames>: Could not allocate memory for new frame\n");
+        return NULL;
+    }
+
+    copy_localsplus(to_push, stack_frame, code->co_nlocalsplus, 0);
+    auto offset = utils::py::get_instr_offset(to_push->frame_obj);
+    
+    stack_frame->owner = to_push->owner;
+    stack_frame->previous = PyEval_GetFrame()->f_frame;
+    stack_frame->f_funcobj = to_push->f_funcobj;
+    stack_frame->f_executable.bits = to_push->f_executable.bits;
+    stack_frame->f_globals = to_push->f_globals;
+    stack_frame->f_builtins = to_push->f_builtins;
+    stack_frame->f_locals = to_push->f_locals;
+    stack_frame->frame_obj = *pyframe_object;
+    stack_frame->stackpointer = stack_frame->localsplus + code->co_nlocalsplus;
+    stack_frame->instr_ptr = (_CodeUnit*) (code->co_code_adaptive + (offset-8));
+    copy_stack(to_push, stack_frame);
+
+    pyframe_object->f_frame = stack_frame;
+    return *pyframe_object;
+}
+
 static PyObject *copy_frame(PyObject *self, PyObject *args) {
     using namespace utils;
     struct _frame *frame = (struct _frame*) PyEval_GetFrame();
@@ -321,6 +348,152 @@ static PyObject* _serialize_frame_from_capsule(PyObject *capsule) {
     return bytes;
 }
 
+static void init_code(PyCodeObject *obj, serdes::DeserializedCodeObject &code) {
+    obj->co_consts = Py_NewRef(code.co_consts.borrow());
+    obj->co_names = Py_NewRef(code.co_names.borrow());
+    obj->co_exceptiontable = Py_NewRef(code.co_exceptiontable.borrow());
+
+    obj->co_flags = code.co_flags;
+    obj->co_argcount = code.co_argcount;
+    obj->co_posonlyargcount = code.co_posonlyargcount;
+    obj->co_kwonlyargcount = code.co_kwonlyargcount;
+    obj->co_stacksize = code.co_stacksize;
+    obj->co_firstlineno = code.co_firstlineno;
+
+    obj->co_nlocalsplus = code.co_nlocalsplus;
+    obj->co_framesize = code.co_framesize;
+    obj->co_nlocals = code.co_nlocals;
+    obj->co_ncellvars = code.co_ncellvars;
+    obj->co_nfreevars = code.co_nfreevars;
+    obj->co_version = code.co_version;
+
+    obj->co_localsplusnames = Py_NewRef(code.co_localsplusnames.borrow());
+    obj->co_localspluskinds = Py_NewRef(code.co_localspluskinds.borrow());
+
+    obj->co_filename = Py_NewRef(code.co_filename.borrow());
+    obj->co_name = Py_NewRef(code.co_name.borrow());
+    obj->co_qualname = Py_NewRef(code.co_qualname.borrow());
+    obj->co_linetable = Py_NewRef(code.co_linetable.borrow());
+
+    memcpy(obj->co_code_adaptive, code.co_code_adaptive.data(), code.co_code_adaptive.size());
+    migrames::PyBitcodeInstruction *bitcode = (migrames::PyBitcodeInstruction *) obj->co_code_adaptive;
+
+    // initialize the rest of the fields
+    obj->co_weakreflist = NULL;
+    obj->co_executors = NULL;
+    obj->_co_cached = NULL;
+    obj->_co_instrumentation_version = 0;
+    obj->_co_monitoring = NULL;
+    obj->_co_firsttraceable = 0;
+    obj->co_extra = NULL;
+}
+
+static PyCodeObject *create_pycode_object(serdes::DeserializedCodeObject& code_obj) {
+    auto code_size = static_cast<Py_ssize_t>(code_obj.co_code_adaptive.size())/2;
+    // NOTE: We're not handling the necessary here when
+    // Py_GIL_DISABLED is defined.
+    PyCodeObject *code = PyObject_NewVar(PyCodeObject, &PyCode_Type, code_size*2);
+    init_code(code, code_obj);
+
+    return code;
+}
+
+// TODO
+static void init_frame(PyFrameObject *frame, py_weakref<PyCodeObject> code, serdes::DeserializedPyFrame& frame_obj) {
+    frame->f_back = NULL;
+    frame->f_frame = NULL;
+    frame->f_trace = NULL;
+    frame->f_extra_locals = NULL;
+    frame->f_locals_cache = NULL;
+
+    frame->f_lineno = frame_obj.f_lineno;
+    frame->f_trace_lines = frame_obj.f_trace_lines;
+    frame->f_trace_opcodes = frame_obj.f_trace_opcodes;
+    
+    if(NULL != *frame_obj.f_trace) {
+        frame->f_trace = Py_NewRef(frame_obj.f_trace.borrow());
+    }
+    if(NULL != *frame_obj.f_extra_locals) {
+        frame->f_extra_locals = Py_NewRef(frame_obj.f_extra_locals.borrow());
+    }
+    if(NULL != *frame_obj.f_locals_cache) {
+        frame->f_locals_cache = Py_NewRef(frame_obj.f_locals_cache.borrow());
+    }
+}
+
+static PyFrameObject *create_pyframe_object(serdes::DeserializedPyFrame& frame_obj, py_weakref<PyCodeObject> code) {
+    // TODO: Double-check that this is the correct size
+    // TODO: What do we do when frame is owned by frame object?
+    // can we just make it owned by thread by construction?
+    // TODO: Should we just make it owned by the frame object?
+    int slots = code->co_nlocalsplus + code->co_stacksize;
+    PyFrameObject *frame = PyObject_GC_NewVar(PyFrameObject, &PyFrame_Type, slots);
+    init_frame(frame, code, frame_obj);
+
+    return frame;
+}
+
+static void init_pyinterpreterframe(migrames::PyInterpreterFrame *interp_frame, 
+                                   serdes::DeserializedPyInterpreterFrame& frame_obj,
+                                   py_weakref<PyFrameObject> frame,
+                                   py_weakref<PyCodeObject> code) {
+    interp_frame->f_globals = NULL;
+    interp_frame->f_builtins = NULL;
+    interp_frame->f_locals = NULL;
+    interp_frame->previous = NULL;
+
+    interp_frame->f_executable.bits = (uintptr_t)Py_NewRef(code.borrow());
+    interp_frame->f_funcobj = Py_NewRef(frame_obj.f_funcobj.borrow());
+
+    if(NULL != *frame_obj.f_globals) {
+        interp_frame->f_globals = Py_NewRef(frame_obj.f_globals.borrow());
+    } else {
+        interp_frame->f_globals = PyEval_GetFrameGlobals();
+
+    }
+    if(NULL != *frame_obj.f_builtins) {
+        interp_frame->f_builtins = Py_NewRef(frame_obj.f_builtins.borrow());
+    } else {
+        interp_frame->f_builtins = PyEval_GetFrameBuiltins();
+    }
+    
+    // These are NOT fast locals, those come from localsplus
+    if(NULL != *frame_obj.f_locals) {
+        interp_frame->f_locals = Py_NewRef(frame_obj.f_locals.borrow());
+    }
+
+    // Here are the locals plus
+    auto localsplus = frame_obj.localsplus;
+    for(size_t i = 0; i < localsplus.size(); i++) {
+        interp_frame->localsplus[i].bits = (intptr_t) Py_NewRef(localsplus[i].borrow());
+    }
+
+    for(size_t i = localsplus.size(); i < code->co_nlocalsplus; i++) {
+        interp_frame->localsplus[i].bits = 0;
+    }
+
+    interp_frame->instr_ptr = (migrames::PyBitcodeInstruction*) 
+        (utils::py::get_code_adaptive(code) + frame_obj.instr_offset/2);
+    auto opcode = interp_frame->instr_ptr->opcode;
+    interp_frame->return_offset = frame_obj.return_offset;
+    interp_frame->stackpointer = interp_frame->localsplus + code->co_nlocalsplus;
+    // TODO: Check what happens when we make the owner the frame object instead of the thread.
+    // Might allow us to skip a copy when calling this frame
+    interp_frame->owner = frame_obj.owner;
+    interp_frame->frame_obj = (PyFrameObject*) Py_NewRef(frame.borrow());
+    frame->f_frame = interp_frame;
+}
+
+static migrames::PyInterpreterFrame *create_pyinterpreterframe_object(serdes::DeserializedPyInterpreterFrame& frame_obj, 
+                                                                      py_weakref<PyFrameObject> frame, 
+                                                                      py_weakref<PyCodeObject> code
+                                                                      ) {
+    migrames::PyInterpreterFrame *interp_frame = NULL;
+    interp_frame = AllocateFrameToMigrate(code->co_framesize);
+    init_pyinterpreterframe(interp_frame, frame_obj, frame, code);
+    return interp_frame;
+}
+
 static PyObject *_deserialize_frame(PyObject *bytes) {
     if(PyErr_Occurred()) {
         PyErr_Print();
@@ -337,7 +510,33 @@ static PyObject *_deserialize_frame(PyObject *bytes) {
     auto deserframe = frame_serdes.deserialize(serframe);
     utils::py::print_object(deserframe.f_frame.f_funcobj.borrow());
 
-    Py_RETURN_NONE;
+    // FRAME_OWNED_BY_THREAD
+    assert(deserframe.f_frame.owner == 0);
+
+    PyCodeObject *code = create_pycode_object(deserframe.f_frame.f_executable);
+    PyFrameObject *frame = create_pyframe_object(deserframe, code);
+    // TODO: Should we just create the interpreter frame on the framestack, instead of doing it here?
+    create_pyinterpreterframe_object(deserframe.f_frame, frame, code);
+
+    return (PyObject*) frame;
+}
+
+
+static PyObject *run_deserialized_frame(PyObject *self, PyObject *args) {
+    PyFrameObject *frame = NULL;
+    if (!PyArg_ParseTuple(args, "O", &frame)) {
+        return NULL;
+    }
+
+    PyThreadState *tstate = PyThreadState_Get();
+    PyCodeObject *code = PyFrame_GetCode(frame);
+    PyFrameObject *to_run = push_frame_for_running(tstate, frame->f_frame, code);
+    if (to_run == NULL) {
+        PySys_WriteStderr("<Migrames>: failed to create frame on the framestack\n");
+        return NULL;
+    }
+    PyObject *res = PyEval_EvalFrame(to_run);
+    return res;
 }
 
 static PyObject *serialize_frame(PyObject *self, PyObject *args) {
@@ -364,6 +563,7 @@ static PyMethodDef MyMethods[] = {
     {"run_frame", run_frame, METH_VARARGS, "Run the frame from the capsule"},
     {"serialize_frame", serialize_frame, METH_VARARGS, "Serialize the frame"},
     {"deserialize_frame", deserialize_frame, METH_VARARGS, "Deserialize the frame"},
+    {"run_deserialized_frame", run_deserialized_frame, METH_VARARGS, "Run the deserialized frame"},
     {NULL, NULL, 0, NULL}
 };
 
